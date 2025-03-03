fb45b2bff0cea7a9c23231e7f0f526a67802914b
augie <46730336+AugieDog08@users.noreply.github.com>
2025-03-03 18:48:39 +0000

    add things

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
-document.addEventListener("resize", () => {
-    resizeCanvas(window.innerWidth, window.innerHeight);
-});
+let lastX = -1;
+let lastY = -1;
+let characters = [];
 
-function preload() {
-    logo = loadImage('/q5js_logo.webp');
-  }
-
-function setup() {
-    createCanvas(200, 100);
+function setup() { // runs first
+    window.addEventListener("resize", resize);
+    window.addEventListener("mouseup", mouseUp);
+    window.addEventListener("mousemove", mouseMove)
+    createCanvas(window.innerWidth, window.innerHeight);
     background('aqua');
-  }
 
-function draw() {
-    background('silver');
-    circle(frameCount % 200, 100, 80);
-  }
+    let temp;
+    for (let row = 0; row < 100; row++) {
+        temp = [];
+        for (let col = 0; col < 50; col++) {
+            temp += "H";
+        }
+        characters += temp;
+    }
+}
+
+function preload() { // runs before setup
+    
+}
+
+function draw() { // runs every frame
+    textSize(10);
+    text(toString());
+}
+
+function resize() {
+    resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+function mouseUp()
+{
+    lastX = -1;
+    lastY = -1;
+}
+
+function mouseMove()
+{
+    if (mouseIsPressed) {
+        strokeWeight(1);
+        stroke("white");
+        if (lastX != -1)
+        {
+            line(lastX, lastY, mouseX, mouseY);
+        }
+        lastX = mouseX;
+        lastY = mouseY;
+    }
+}
+
+function toString() {
+    s = "";
+    for (let i = 0; i < characters.length; i++) {
+        s += characters[i].join();
+    }
+    return s;
+}
