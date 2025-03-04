398397f95d87063a82c259a59cd623de23c5046d
augie <46730336+AugieDog08@users.noreply.github.com>
2025-03-04 13:39:58 +0000

    add comments

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+class Element {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Draw extends Element {
+    constructor(x, y) {
+        super(x, y);
+    }
+}
+
 let lastX = -1;
 let lastY = -1;
 let characters = [];
@@ -59,3 +72,60 @@ function toString() {
     }
     return s;
 }
+
+// 0001: .
+//  next subpixel is up right: ,
+// 0010: .
+// 0011: _
+//  if next char is down right and 1001: .
+// 0100: '
+//  if next subpixel is down right: `
+// 0101: |
+// 0110: /
+//  if above char is 1010 or 0101: '
+//  if right char is 1100: .
+// 1000: '
+// 1001: \
+//  if above char is 1010 or 0101: '
+//  if left char is 1100: .
+// 1010: |
+// 1100: -
+//  if last subpixel was top left: `
+
+//   ___          
+// ,'   `.        
+//        \       
+//         |      
+//         '.     
+//          |     
+//          `.    
+//            `-._
+
+// 0001
+// 0110
+// 0011
+// 0011
+// 0011
+// 1100
+// 0011
+// 1001
+// 1010
+// 1001
+// 0010
+// 1010
+// 0100
+// 0010
+// 1100
+// 1100
+// 1001
+// 0011
+
+//   ___          
+// ./   -_        
+//        \       
+//         |      
+//         \.     
+//          |     
+//          '.    
+//            --\_
+
